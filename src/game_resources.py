209ece83b61087fda672b95048c83af71f109559
Dmitry Kutsenko <d2emonium@gmail.com>
2025-01-14 15:25:39 +0300

    Fix lint warnings

--- src/game_resources.py
+++ src/game_resources.py
@@ -1,24 +1,62 @@
+"""Load resources.
+
+Classes:
+
+    GameResources
+"""
+
 import logging
 import pygame
 from sprite_loader import load_logos, load_portraits
 
 
 class GameResources:
+    """Resource loader."""
+
     __instance = None
+    """Resources instance."""
     __is_loaded = False
-    __resources = {}
+    """Resource list is loaded."""
+    resources = {}
+    """Resources dictionary."""
 
     def __init__(self):
         logging.debug("Создание реестра ресурсов")
         GameResources.__instance = self
         self.load()
 
-    def __load(self):
-        if self.__is_loaded:
+    @classmethod
+    def instance(cls):
+        """Get resouces instance.
+
+        Returns:
+            GameResources: Resources instance.
+        """
+        if cls.__instance is None:
+            cls()
+
+        return cls.__instance
+
+    @classmethod
+    def get(cls, resource_id):
+        """Get resource by id.
+
+        Args:
+            resource_id (string): Id of resource.
+
+        Returns:
+            any: Resource.
+        """
+        return cls.instance().resources.get(resource_id)
+
+    @classmethod
+    def load(cls):
+        """Load resources list."""
+        if cls.__is_loaded:
             return
 
         logging.debug("Загрузка ресурсов")
-        self.__resources = {
+        cls.resources = {
             # Images
             'main-screen': pygame.image.load("res/main-screen.png").convert_alpha(),
             # Spritesheets
@@ -30,23 +68,4 @@ class GameResources:
         }
 
         logging.debug("Ресурсы загружены")
-        self.__is_loaded = True
-
-    def __get(self, resource_id):
-        return self.__resources.get(resource_id)
-
-    @classmethod
-    def instance(cls):
-        if cls.__instance is None:
-            cls()
-
-        return cls.__instance
-
-    @classmethod
-    def load(cls):
-        cls.instance().__load()
-
-    @classmethod
-    def get(cls, resource_id):
-        return cls.instance().__get(resource_id)
-
+        cls.__is_loaded = True
