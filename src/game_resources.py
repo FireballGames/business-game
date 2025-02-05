fa4c3e2606157dc4742e51281efc91fe8b98fdd9
Dmitry Kutsenko <d2emonium@gmail.com>
2025-02-05 17:32:46 +0300

    Log resources

--- src/game_resources.py
+++ src/game_resources.py
@@ -10,6 +10,9 @@ import pygame
 from sprite_loader import load_logos, load_portraits
 
 
+logger = logging.getLogger('resource')
+
+
 class GameResources:
     """Resource loader."""
 
@@ -21,7 +24,7 @@ class GameResources:
     """Resources dictionary."""
 
     def __init__(self):
-        logging.debug("Создание реестра ресурсов")
+        logger.debug("Создание реестра ресурсов")
         GameResources.__instance = self
         self.load()
 
@@ -50,26 +53,39 @@ class GameResources:
         return cls.instance().resources.get(resource_id)
 
     @classmethod
+    def __load_resource(cls, resource_id, data):
+        cls.resources[resource_id] = data
+        logger.debug(f"Ресурс \"{resource_id}\" загружен")
+
+
+    @classmethod
     def load(cls):
         """Load resources list."""
         if cls.__is_loaded:
             return
 
-        logging.debug("Загрузка ресурсов")
-        cls.resources = {
-            # Images
-            'main-screen': pygame.image.load("res/main-screen.png").convert_alpha(),
-            # Spritesheets
-            'logos': load_logos(),
-            'portraits': load_portraits(),
-            # Fonts
-            'header_font': pygame.font.Font("res/fonts/OldStandardTT-Regular.ttf", 48),
-            'big_font': pygame.font.Font("res/fonts/OldStandardTT-Regular.ttf", 36),
-            'small_font': pygame.font.Font("res/fonts/OldStandardTT-Regular.ttf", 24),
-            # Controls
-            'property-button': pygame.image.load("res/property-button.png").convert_alpha(),
-            'portrait-button': pygame.image.load("res/portrait.png").convert_alpha(),
-        }
+        logger.debug("Загрузка ресурсов")
+        cls.resources = {}
 
-        logging.debug("Ресурсы загружены")
+        # Images
+        logger.debug("Загрузка изображений")
+        cls.__load_resource('main-screen', pygame.image.load("res/main-screen.png").convert_alpha())
+
+        # Spritesheets
+        logger.debug("Загрузка спрайтлистов")
+        cls.__load_resource('logos', load_logos())
+        cls.__load_resource('portraits', load_portraits())
+
+        # Fonts
+        logger.debug("Загрузка шрифтов")
+        cls.__load_resource('header_font', pygame.font.Font("res/fonts/OldStandardTT-Regular.ttf", 48))
+        cls.__load_resource('big_font', pygame.font.Font("res/fonts/OldStandardTT-Regular.ttf", 36))
+        cls.__load_resource('small_font', pygame.font.Font("res/fonts/OldStandardTT-Regular.ttf", 24))
+
+        # Controls
+        logger.debug("Загрузка элементов управления")
+        cls.__load_resource('property-button', pygame.image.load("res/property-button.png").convert_alpha())
+        cls.__load_resource('portrait-button', pygame.image.load("res/portrait.png").convert_alpha())
+
+        logger.debug("Ресурсы загружены")
         cls.__is_loaded = True
